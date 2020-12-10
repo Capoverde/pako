b03160b63551cd8c476d59690c5027a63dae0eaf
Piotr <piotr.slobinski@gmail.com>
2020-12-11 00:07:43 +0100

    nav toggling on rwd handled

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,11 +1,14 @@
 const menuBtn = document.querySelector('.menu-btn')
+const navList = document.querySelector('.page__nav-list')
 let menuOpen = false
 menuBtn.addEventListener('click', () => {
   if (!menuOpen) {
     menuBtn.classList.add('open')
+    navList.classList.add('navOpen')
     menuOpen = true
   } else {
     menuBtn.classList.remove('open')
+    navList.classList.remove('navOpen')
     menuOpen = false
   }
 })
