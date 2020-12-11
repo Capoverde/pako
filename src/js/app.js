90af7735d41cb0fbf81a6b7f2d132293ec9cd0d6
Piotr <piotr.slobinski@gmail.com>
2020-12-11 19:39:00 +0100

    slider-opinion styling + smoothscroll to section

--- src/js/app.js
+++ src/js/app.js
@@ -22,7 +22,7 @@ var swiper = new Swiper('.swiper-container', {
   spaceBetween: 20,
   arrows: true,
   loop: true,
-  autoplay: true,
+  // autoplay: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true
@@ -34,7 +34,7 @@ var swiper = new Swiper('.swiper-opinion-container', {
   spaceBetween: 20,
   arrows: true,
   loop: true,
-  autoplay: true,
+  // autoplay: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true
@@ -102,4 +102,30 @@ $(function () {
   //   }
   //   $('.num1').html(start)
   // }, speed)
+
+
+  // smooth scroll to section:
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 })
